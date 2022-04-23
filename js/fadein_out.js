28a75dcca94963bee3b0a3f92ccdb2ee8ee4198d
Renzo Carrazzoni <carrazzoni.renzo@gmail.com>
2022-04-23 18:11:30 -0300

    Implementando diseño recursivo

--- js/fadein_out.js
+++ js/fadein_out.js
@@ -9,12 +9,20 @@ function invocarFade(input){
         fadeOut(sinTexto);
         fadeOut(sinTextoImg);
         fadeIn(botonCopiar);
+        if(window.innerWidth<480){
+            mostrarRecursivo();
+        }
         botonCopiar.classList.remove("invisible");
     }
     else{
         fadeIn(sinTexto);
         fadeIn(sinTextoImg);
         fadeOut(botonCopiar);
+
+        if(window.innerWidth<480){
+            regresarRecursivo();
+        }
+
         botonCopiar.classList.add("invisible");
     }
 
@@ -32,4 +40,27 @@ function fadeIn(elemento){
     elemento.classList.remove("fadeOut");
     elemento.classList.add("fadeIn");
     return;
+}
+
+function mostrarRecursivo(){
+    document.querySelector(".rectangle").style.height = "595px";
+
+    document.querySelector(".textoModificado").style.height = "432px"
+    document.querySelector(".textoModificado").style.margin = "32px 0 32px -132.5px"
+
+    document.querySelector(".frame2").style.height = "67px";
+    document.querySelector(".frame2").style.width = "279px";
+    document.querySelector(".frame2").style.margin = "342px auto 0 auto";
+    return;
+}
+
+function regresarRecursivo(){
+    document.querySelector(".rectangle").style.height = "186px";
+
+    document.querySelector(".textoModificado").style.height = "186px"
+    document.querySelector(".textoModificado").style.margin = "0px 0 0px -132.5px"
+
+    document.querySelector(".frame2").style.height = "0px";
+    document.querySelector(".frame2").style.width = "0px";
+    document.querySelector(".frame2").style.margin = "0px";
 }
